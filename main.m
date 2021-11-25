ca8dc3166ad2f9685e524f3ccc899485dc8f4851
Jasper Boogers <jasperboogers@gmail.com>
2021-11-25 11:14:52 +0100

    update gitignore, vraag 2 toegevoegd

--- main.m
+++ main.m
@@ -13,8 +13,26 @@ T_rated = 1E4; % Nm
 W_rated = 180; % rad/s
 V_lin = 16; % m/s
 
+% set graphics options
+opt = bodeoptions;
+opt.PhaseWrapping = 'on';
+
 %% SISO controller
-SISO_ss = -FWT(1,1); % Beta to Omega
-figure(1);
-subplot(1,2,1); margin(SISO_ss);
-subplot(1,2,2); pzmap(SISO_ss);
+
+% plant
+G = FWT(1,1); % Beta to Omega
+% figure(1);
+% subplot(1,2,1); margin(G,opt);
+% subplot(1,2,2); pzmap(G);
+
+% controller
+K = pid(0,-0.25,0, 'InputName', 'Error (rad/s)', 'OutputName', 'Beta (deg)');
+L = feedback(G*K,1);
+S = 1/(1+L);
+T = L/(1+L);
+
+figure(2);
+subplot(2,2,1); bode(L,opt); title('Controller TF');
+subplot(2,2,2); bodemag(S); title('Sensitivity');
+subplot(2,2,3); bodemag(T); title('Complementary Sensitivity');
+subplot(2,2,4); step(L); stepinfo(L)
