03f35af37fd17ba4446aeb0e26d2e74c1fc63ed4
Jasper Boogers <jasperboogers@gmail.com>
2021-12-01 15:25:55 +0100

    comments toegevoegd

--- main.m
+++ main.m
@@ -40,13 +40,18 @@ L_mimo = feedback(FWT, K, [1], [1], -1);
 figure(3); step(L_mimo(1,3));
 
 %% Mixed Sensitivity Design
+
+% obtain plant
 MIMO_ss = minreal(tf(FWT(1:2,1:2)));
+
+% calculate RGA
 RGA = MIMO_ss.*inv(MIMO_ss);
 [U,S,V] = svd(evalfr(RGA, 0.8*pi));
 
 MIMO_zeros = tzero(MIMO_ss);
 MIMO_poles = pole(MIMO_ss);
 
+% build W matrices and generalized plant
 Wp11 = tf([1/1.8, 0.8*pi], [1, 0.8*pi*1e-4]);
 Wu22 = tf([5e-3, 7e-4, 5e-5], [1, 14e-4, 1e-6]);
 Wp = [Wp11 0; 0 0.2];
@@ -54,6 +59,7 @@ Wu = [0.01 0; 0 Wu22];
 P11=[Wp; zeros(2,2)]; P12=[Wp*MIMO_ss; Wu]; P21=eye(2); P22=MIMO_ss;
 P = [P11 P12; P21 P22];
 
+% H-infinity norm synthesis
 [K, CL, gamma] = hinfsyn(P, 2, 2);
 L_ref = feedback(MIMO_ss, K, [1,2], [1,2], -1);
 L_dist = feedback(FWT, K, [1,2], [1,2], -1);
