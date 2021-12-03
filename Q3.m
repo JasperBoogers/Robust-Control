165310852bf60d0f70c2e9a05a7273277609f7d2
Jasper Boogers <jasperboogers@gmail.com>
2021-12-03 18:13:19 +0100

    setup voor vraag 3, G nog aanpassen

---
+++ Q3.m
@@ -0,0 +1,32 @@
+clear all
+clc
+
+%% parameters
+
+% load ss
+load('Assignment_Data_SC42145.mat');
+
+% operational parameters
+P_rated = 1.8E6; % W
+T_rated = 1E4; % Nm
+W_rated = 180; % rad/s
+V_lin = 16; % m/s
+
+s = tf('s');
+
+%% Matrix setup
+G = minreal(FWT);
+
+w_hpf = 1;
+w_lpf= 1e-2;
+LPF = tf(w_lpf, [1, w_lpf]);
+HPF = tf([1,0],[1, w_hpf]);
+
+M = 1;
+w_b = 1;
+A = 1e-4;
+
+
+Wu = [LPF, 0; 0 HPF];
+Wp = tf([1/M, w_b], [1, w_b*A]);
+P = [Wp Wp*G; zeros(2) Wu; 1 -G];
