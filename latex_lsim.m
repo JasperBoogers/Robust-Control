f0b308fb9528658f7261f109d7053b3a94dd7427
gerben2009 <gerben2009@gmail.com>
2021-12-08 14:31:06 +0100

    boemmm

--- latex_lsim.m
+++ latex_lsim.m
@@ -1,6 +1,61 @@
-function latex_lsim(sys,u,t)
+function latex_lsim(sys,u,t,grid_format,u_2)
 %UNTITLED Summary of this function goes here
 %   Detailed explanation goes here
-    y = lsim(closed_loop(:,2), u,t);
+    y = lsim(sys, u,t);    
+    if nargin == 5
+        y2 = lsim(sys,u_2,t);
+    end
+
+    n_outputs = size(y,2);
+    
+    
+    ex_inputs = grid_format(1); %amount of exogenous inputs
+    grid_x = ex_inputs;
+    grid_y = n_outputs-grid_x+1;
+    
+    assert(sum(grid_format)==n_outputs);
+    
+    for i_plot = 1:n_outputs %amount of exogenous inputs
+        if i_plot <= grid_format(1)
+            subplot(grid_y,grid_x, i_plot);
+            colr = "blue";
+            labl = 'Input ';
+        elseif i_plot <= grid_format(1)+grid_format(2)
+            i_grid = grid_format(1)+1+(i_plot-1-grid_format(1))*2;
+            subplot(grid_y,grid_x, [i_grid i_grid+1]);
+            colr = "red";
+            labl = 'Output ';
+        else
+            i_grid = grid_x*grid_y-grid_format(3)*2+1 +2*(i_plot-1-grid_format(1)-grid_format(2));
+
+            subplot(grid_y,grid_x, [i_grid i_grid+1]);
+            colr = 'magenta';
+            labl = 'Control ';
+        end
+        plot(t,y(:,i_plot))
+        if nargin==5
+            hold on
+            plot(t,y2(:,i_plot))
+            if i_plot ==2
+                legend("Simulation 1", "Simulation 2")
+        end
+        grid on
+        if (i_plot<n_outputs)&&(i_plot>grid_format(1))
+        set(gca,'xticklabel',[])
+        else
+            xlabel('Time (seconds)')
+        end
+        outputName = sys.OutputName(i_plot);
+        outputName = replace(outputName,"tau_e","$\tau_e$");
+        outputName = replace(outputName,"Omega","$\omega_r$");
+        outputName = replace(outputName,"Beta","$\beta$");
+        
+
+        ylabel(append(labl,outputName),'interpreter', 'latex', "Color", colr);
+    end
+  %  subplot(grid_y,grid_x,1)
+   % title("\textbf{Simulation results}" ,'interpreter', 'latex');
+
+    sgtitle('Simulation results') 
 end
 
