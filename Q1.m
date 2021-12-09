b0a5daddf87f6b16250998bd684ff398b3295aaf
Jasper Boogers <jasperboogers@gmail.com>
2021-12-09 16:03:55 +0100

    refactor Q1.m

--- Q1.m
+++ Q1.m
@@ -17,13 +17,13 @@ s = tf('s');
 
 %% SISO controller
 
-% plant
+% define plant
 G = FWT(1,1); % Beta to Omega
 figure(1);
 subplot(1,2,1); margin(G);
 subplot(1,2,2); pzmap(G);
 
-% controller
+% build controller
 wn=0.199;
 K1 = -0.39*tf([25,1,1],1); % complex zero at 0.2 rad for increased phase
 K2 = tf(1,[1,0]); % integrator for reference tracking
@@ -31,13 +31,18 @@ K3 = tf(1, [1,1]); % real pole for noise rejection
 notch = tf([(1/(wn))^2, 1/(wn*10), 1], [(1/(wn))^2, 1/(wn*25), 1]);
 
 K = pid(K1*K2*K3);
+
+% add in/outputnames
 K.InputName = 'Error (rad/s)';
 K.OutputName = 'Beta (deg)';
+
+% build closed loop
 L = K*G;
 CL = feedback(L, 1);
 S = 1/(1+L);
 T = L/(1+L);
 
+% make figures
 figure(); latex_bode(CL, 'the loop transfer function');
 saveas(gcf, 'Figures/ch1_loop_tf.eps',  'epsc')
 figure(); latex_bodemag(S, 'the Sensitivity');
@@ -49,7 +54,7 @@ saveas(gcf, 'Figures/ch1_step.eps',  'epsc')
 
 [GM, PM, Wcg, Wcross] = margin(L);
 
-% put controller in MIMO system
+% put controller in MIMO system and perform step response on 3rd input channel
 L_mimo = feedback(FWT, K, [1], [1], -1);
 figure(); latex_step(L_mimo(1,3), 'Disturbance Rejection of V');
 saveas(gcf, 'Figures/ch1_disturbance.eps',  'epsc')
