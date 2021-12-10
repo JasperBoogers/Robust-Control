7c7018605cf7c83c8230c69435ec5e51ec3d52fd
gerben2009 <gerben2009@gmail.com>
2021-12-10 11:13:47 +0100

    fix

--- latex_step.m
+++ latex_step.m
@@ -1,6 +1,10 @@
 function [] = latex_step(sys,plot_title)
 set(groot,'defaulttextinterpreter','latex');  
 set(groot, 'defaultAxesTickLabelInterpreter','latex'); 
+sys.OutputName = replace(sys.OutputName,"Omega","$\omega$");
+sys.OutputName = replace(sys.OutputName,"tau_e","$\tau_e$");
+sys.OutputName = replace(sys.OutputName,"Beta","$\beta$");
+
 step(sys);
 warning off
 title(append('\textbf{Step plot of ',plot_title,'}'),'interpreter', 'latex');
@@ -10,5 +14,12 @@ ylabel('Amplitude','interpreter', 'latex');
 hs = findall(gcf, 'type', 'axes');
 ha = get(hs(2), 'title');
 ha.String = ''; % remove the subtitle "From: u1 To: y1"
+
+%axs= findobj(allchild(gcf), 'flat', 'Type', 'axes');
+for i_ax = 1:numel(hs)
+    hs(i_ax).YLabel.Interpreter = 'latex';
+    ha = get(hs(i_ax), 'title');
+    ha.Interpreter = 'latex';
 end
 
+end
