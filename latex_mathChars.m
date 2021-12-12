dd043c94f4694d8fd353b3601a543c6e9bad827a
Jasper Boogers <jasperboogers@gmail.com>
2021-12-12 13:51:41 +0100

    nog een dollarteken vergeten

--- latex_mathChars.m
+++ latex_mathChars.m
@@ -1,5 +1,5 @@
 function output = latex_mathChars(input)
-    output = replace(input,"Beta",'$\beta');
+    output = replace(input,"Beta",'$\beta$');
     output = replace(output,"tau_e","$\tau_e$");
     output = replace(output,"Omega","$\omega$");
 
